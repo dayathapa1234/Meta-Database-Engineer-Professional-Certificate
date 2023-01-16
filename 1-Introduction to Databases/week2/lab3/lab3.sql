fc9c6e84958da082704e1f63578636b8ddcf747c
dayathapa1234 <60889983+dayathapa1234@users.noreply.github.com>
2023-01-16 00:00:27 +0000

    Update lab3.sql

--- 1-Introduction to Databases/week2/lab3/lab3.sql
+++ 1-Introduction to Databases/week2/lab3/lab3.sql
@@ -1,1 +1,7 @@
-
+mysql;
+CREATE DATABASE cm_devices;
+USE cm_devices;
+CREATE TABLE address (id int NOT NULL,  street VARCHAR(255), postcode VARCHAR(10), town VARCHAR(30) DEFAULT "Harrow");
+Show columns from address; 
+DROP TABLE Address;
+CREATE TABLE Address (id int NOT NULL,  street VARCHAR(255), postcode VARCHAR(10) DEFAULT "HA97DE", town VARCHAR(30) DEFAULT "Harrow");
