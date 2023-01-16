a267bd4689a06e893f621a063af1d38ddf9eb901
dayathapa1234 <60889983+dayathapa1234@users.noreply.github.com>
2023-01-16 18:58:22 +0000

    Update lab6.sql

--- 1-Introduction to Databases/week2/lab6/lab6.sql
+++ 1-Introduction to Databases/week2/lab6/lab6.sql
@@ -1,1 +1,6 @@
-
+mysql;
+CREATE DATABASE football_club;
+USE football_club;
+CREATE TABLE players(playerID INT, playerName VARCHAR(50), age INT);
+SHOW tables;
+CREATE TABLE games(gameID INT, gameDate DATE, score INT);   
