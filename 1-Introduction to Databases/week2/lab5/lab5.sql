db3b48cc79405de56986d1d881892b57f7bdf7c7
dayathapa1234 <60889983+dayathapa1234@users.noreply.github.com>
2023-01-16 13:33:11 +0000

    Update lab5.sql

--- 1-Introduction to Databases/week2/lab5/lab5.sql
+++ 1-Introduction to Databases/week2/lab5/lab5.sql
@@ -1,1 +1,9 @@
-
+mysql;
+CREATE DATABASE bookshop;
+USE bookshop;
+CREATE TABLE customers(customerID INT, customerName VARCHAR(50), customerAddress VARCHAR(255));
+SHOW tables;
+INSERT INTO customers (customerID, customerName, customerAddress) VALUES (1,"Daya","190 Wilkins Road");
+SELECT * FROM customers;
+INSERT INTO customers(customerID, customerName, customerAddress) VALUES (2, "James", "24 Carlson Rd London")  
+SELECT * FROM customers;
