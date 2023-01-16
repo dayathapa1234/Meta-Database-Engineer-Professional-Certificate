b1a2f5f2e1ab3fe62e1deb5f2f0e3adc48ce274f
dayathapa1234 <60889983+dayathapa1234@users.noreply.github.com>
2023-01-16 00:17:11 +0000

    Update lab4.sql

--- 1-Introduction to Databases/week2/lab4/lab4.sql
+++ 1-Introduction to Databases/week2/lab4/lab4.sql
@@ -1,1 +1,7 @@
-
+mysql;
+CREATE DATABASE cm_devices; 
+USE cm_devices;
+CREATE TABLE invoice(customerID VARCHAR(50), orderDate DATE, quantity INT, price DECIMAL);
+SHOW tables;
+Show columns from invoice; 
+CREATE TABLE customer_details(accountNumber INT(10), phoneNumber INT(11), email VARCHAR(50));
